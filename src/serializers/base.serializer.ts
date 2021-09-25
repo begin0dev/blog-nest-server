367e0e5ba28dfa8cb99d0c9b4edae48f5066a8b7
BEGIN0DEV <prosonic1@gmail.com>
2021-09-25 17:05:24 +0900

    FIX: _id to string

--- src/serializers/base.serializer.ts
+++ src/serializers/base.serializer.ts
@@ -2,6 +2,8 @@ import { Expose, Transform } from 'class-transformer';
 
 export class BaseSerializer {
   @Expose()
-  @Transform((value) => value.toString())
+  @Transform((value) => {
+    return typeof value.obj._id === 'string' ? value.obj._id : value.obj._id.toString();
+  })
   _id: string;
 }
